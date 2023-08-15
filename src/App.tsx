495fc0bcd7d4e600a0160612ef1bf63bd8838769
lamisab <l.alhubishi@thegarage.sa>
2023-08-15 11:47:25 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <div className="bg-white rounded shadow p-6">
+        <h1 className="text-2xl font-bold mb-4">Welcome to React with Tailwind CSS!</h1>
+        <p className="text-gray-600">This is a sample component styled with Tailwind CSS.</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
